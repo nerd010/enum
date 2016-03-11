a02ba8bb324510d60f15f90ccb8227d48e8ca288
Charles Wang <Charles@wangzengkangdemacbook-pro.local>
2016-03-11 08:21:31 +0800

    Initial Commit

---
+++ Enum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Enum
+//
+//  Created by Charles Wang on 16/3/11.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
